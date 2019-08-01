fd5192cbf25929cfe6ceee8f56f40095f023e602
Luke Pickering <luketpickering@googlemail.com>
2019-08-01 14:39:28 -0400

    Adds NEUTClass linkdef

---
+++ src/neutclass/neutclassLinkDef.h
@@ -0,0 +1,13 @@
+#ifdef __CINT__
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+#pragma link C++ class NeutCtrl;
+#pragma link C++ class NeutFsiPart;
+#pragma link C++ class NeutFsiVert;
+#pragma link C++ class NeutNucFsiStep;
+#pragma link C++ class NeutNucFsiVert;
+#pragma link C++ class NeutPart;
+#pragma link C++ class NeutVect;
+#pragma link C++ class NeutVtx;
+#endif
