5e3b78173e49755fd9dd7aa94def85eef53132bd
Luke Pickering <luketpickering@googlemail.com>
2019-08-15 14:42:17 -0500

    Uses abstract elaxff method everywhere uniformly now (qedifcrs, SF, NCELQ2). Clarifies many variable names with units. Ports forward SF MAQE reweighting fixes

--- src/neutcore/qedifcrs.c
+++ src/neutcore/qedifcrs.c
@@ -1,78 +1,30 @@
-#include <stdlib.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 
+#include "necardC.h"
 #include "neutmodelC.h"
 #include "neutparamsC.h"
-#include "necardC.h"
 #include "rsconsC.h"
 
+#ifdef __cplusplus
+extern "C" {
+double elaxff_(double *Q2_GeV, bool *nc, int *sgn);
+}
+#else
+double elaxff_(double *Q2_GeV, _Bool *nc, int *sgn);
+#endif
 
-
-/*  for 64 bits OS & g77 compiler -- M. Fechner*/
-//#if defined(f2cFortran)&&!defined(gFortran)
-//#define FUNCTION_RETURN double
-//#else
-//#define FUNCTION_RETURN float
-//#endif
-
-//extern "C" {
-  //FUNCTION_RETURN bbba05_(double* Q2,double* GPE,double* GPM,double* GNE,double* GNM);
-  double bbba05_(double* Q2,double* GPE,double* GPM,double* GNE,double* GNM);
-  double bbba07_(double* Q2,double* GPE,double* GPM,double* GNE,double* GNM);
-
-//}
-
-/*
-extern struct nemdls_common {
-  int    mdlqe;
-  int    mdlspi;
-  int    mdldis;
-  int    mdlcoh;
-  int    mdlqeaf;
-  float  xmaqe;
-  float  xmaspi;
-  float  xmvqe;
-  float  xmvspi;
-  float  kapp;
-} nemdls_;
-
-extern struct nenupr_common {
-  float  pfsurf;
-  float  pfmax;
-  float  vnuini;
-  float  vnufin;
-  int    iformlen;
-} nenupr_;
-
-extern struct neuttarget_common {
-  int    numbndn;
-  int    numbndp;
-  int    numfrep;
-  int    numatom;
-} neuttarget_;
-*/
-
-extern double faBBBA07(const double);
-extern double fa2COMP(const double);
-extern double fa3COMP(const double);
-extern double faZEXP(const double);
-
-double
-qedifcrs_(int *ip, int *ccnc, float *energy, float *elep, float *coslep)
-{
-  /* All in MeV */
+double qedifcrs_(int *ip, int *iscc, float *energy_GeV, float *elep_GeV,
+                 float *coslep) {
+  /* All in GeV */
 
   //--- Parameters
 
-  //  double const PI = 3.1415926;
-  //  double const HC = 197.326;  // MeV fm
-  double const HC = 0.197326;   // GeV fm
+  double const HC = 0.197326; // GeV fm
   double const GF = 1.1664e-11;
   double const COSB2 = .9494;
-  //  double const XMNc = XMN*1000;  // p/n average
-  double const XMNc = XMN;  // p/n average
-  //double XMNc;
+  double const XMNc = XMN; // p/n average
   double const DMN = 0.;
   double const EBF = 0.;
 
@@ -81,181 +33,170 @@ qedifcrs_(int *ip, int *ccnc, float *energy, float *elep, float *coslep)
 
   int mec = 0;
 
-  //  float Maxial = nemdls_.xmaqe*1000;
-  //  float Mvec   = nemdls_.xmvqe*1000;
   float Maxial = nemdls_.xmaqe;
-  float Mvec   = nemdls_.xmvqe;
-  float kappa  = nemdls_.kapp;
-
-  int FF_model = nemdls_.mdlqeaf;
+  float Mvec = nemdls_.xmvqe;
+  float kappa = nemdls_.kapp;
 
   float fperr = nemdls_.fpqe;
 
-  //  float PFermi = nenupr_.pfsurf*1000;
-  //  float Ebind  = nenupr_.vnuini*(-1000);
   float PFermi = nenupr_.pfsurf;
-  float Ebind  = -nenupr_.vnuini;
-  
-  float Nnum   = neuttarget_.numbndn;
-  float Znum   = neuttarget_.numbndp;
-  
+  float Ebind = -nenupr_.vnuini;
+
+  float Nnum = neuttarget_.numbndn;
+  float Znum = neuttarget_.numbndp;
+
   float sccv = nemdls_.sccfv;
   float scca = nemdls_.sccfa;
-  
+
   //--- Local variables
-  double W,Q,Q2,PLEP,WEF,QVEC2,AP,BP,Q2EF;
-  double FGE,FGM,F1,F2,FA,FP,FV3,FA3;
-  double  GPE,GPM,GNE,GNM;
-  double T1,T2,TA,TB,T8;
-  double W1,W2,WA,WB,W8;
-  double A1,A2,A3,A4,A5,A6,A7;
-  double B0,B1,B2;
-  double C,D,X0,DBLS,S1,S2,EU,EL;
+  double W_GeV, Q_GeV, Q2_GeV2, PLEP, WEF, QVEC2, AP, BP, Q2EF;
+  double FGE, FGM, F1, F2, FA, FP, FV3, FA3;
+  double GPE, GPM, GNE, GNM;
+  double T1, T2, TA, TB, T8;
+  double W1, W2, WA, WB, W8;
+  double A1, A2, A3, A4, A5, A6, A7;
+  double B0, B1, B2;
+  double C, D, X0, DBLS, S1, S2, EU, EL;
   double DBLQ2;
   double COX, DSIG;
   double PFn;
 
-  int    NUSIG;
+  int nupdgsign;
 
-  double XM=0;
+  double Mlep_GeV = 0;
 
   DSIG = 0.;
   EL = 0.;
 
-  if (*ccnc != 0){
-	if (abs(*ip)==12){
-	  //	  XM = XME*1000;
-	  XM = XME;
-	}else if (abs(*ip)==14){
-	  //	  XM = XMMU*1000;
-	  XM = XMMU;
-	}else if (abs(*ip)==16){
-	  //	  XM = XMTAU*1000;
-	  XM = XMTAU;
-	}
-  }else{
-	XM = 0.;
+#ifdef __cplusplus
+  bool isnc;
+#else
+  _Bool isnc;
+#endif
+
+  if (*iscc) {
+    isnc = 0;
+    if (abs(*ip) == 12) {
+      Mlep_GeV = XME;
+    } else if (abs(*ip) == 14) {
+      Mlep_GeV = XMMU;
+    } else if (abs(*ip) == 16) {
+      Mlep_GeV = XMTAU;
+    }
+  } else {
+    isnc = 1;
+    Mlep_GeV = 0.;
   }
 
-  if (*ip>0){
-	NUSIG=1;
-	//XMNc = XMNE;
-  }else{
-	NUSIG=-1;
-	//XMNc = XMP;
+  if (*ip > 0) {
+    nupdgsign = 1;
+  } else {
+    nupdgsign = -1;
   }
 
-  if ((*elep) < XM) return 0.;
+  if ((*elep_GeV) < Mlep_GeV) {
+    return 0;
+  }
 
   //--- Kinematical variables
-  W = *energy - (*elep);
-  if (W <= 0.) return 0.;
-  PLEP = sqrt(((*elep))*((*elep)) - XM*XM);
-  EU = sqrt((PFermi)*(PFermi) + XMNc*XMNc);
-  WEF = W + EBF - (Ebind);
-  AP = (Ebind)*(1. + EBF/W);
-  BP = EBF*(1. - (Ebind)/W);
-  DBLQ2 = ((*energy)*(*energy) + PLEP*PLEP - 2.*(*energy)*PLEP*(*coslep));
-  if ( DBLQ2 < 0. && DBLQ2 >= -1.e-7) DBLQ2 = 0.;
-  Q  = sqrt(DBLQ2);
-  QVEC2 = Q*Q;
-  Q2 = Q*Q - W*W;
-  Q2EF = Q*Q - WEF*WEF - DMN;
+  W_GeV = *energy_GeV - (*elep_GeV);
+  if (W_GeV <= 0) {
+    return 0;
+  }
+  PLEP = sqrt(((*elep_GeV)) * ((*elep_GeV)) - Mlep_GeV * Mlep_GeV);
+  EU = sqrt((PFermi) * (PFermi) + XMNc * XMNc);
+  WEF = W_GeV + EBF - (Ebind);
+  AP = (Ebind) * (1. + EBF / W_GeV);
+  BP = EBF * (1. - (Ebind) / W_GeV);
+  DBLQ2 = ((*energy_GeV) * (*energy_GeV) + PLEP * PLEP -
+           2. * (*energy_GeV) * PLEP * (*coslep));
+  if (DBLQ2 < 0. && DBLQ2 >= -1.e-7)
+    DBLQ2 = 0.;
+  Q_GeV = sqrt(DBLQ2);
+  QVEC2 = Q_GeV * Q_GeV;
+  Q2_GeV2 = Q_GeV * Q_GeV - W_GeV * W_GeV;
+  Q2EF = Q_GeV * Q_GeV - WEF * WEF - DMN;
 
   //--- Form factor
-  if ((nemdls_.mdlqe%10)==2) {
-    //printf("qedifcrs.c Q2 = %g\n",Q2);
-    bbba05_(&Q2,&GPE,&GPM,&GNE,&GNM);
-    //printf("qedifcrs.c GPE GPM GNE GNM = %g %g %g %g\n",GPE   , GPM  ,  GNE, GNM);
-    FGE=GPE-GNE;
-    FGM=GPM-GNM;
-  }
-  else if ((nemdls_.mdlqe%10)==3) {
-    //printf("qedifcrs.c Q2 = %g\n",Q2);
-    bbba07_(&Q2,&GPE,&GPM,&GNE,&GNM);
-    //printf("qedifcrs.c GPE GPM GNE GNM = %g %g %g %g\n",GPE   , GPM  ,  GNE, GNM);
-    FGE=GPE-GNE;
-    FGM=GPM-GNM;
-  }
-  else if ((nemdls_.mdlqe%10)==1) {
-    FGE = pow(1. + Q2/((Mvec)*(Mvec)), -2.);
-    FGM = (1. + 3.71)*FGE;
+  if ((nemdls_.mdlqe % 10) == 2) {
+    // printf("qedifcrs.c Q2_GeV2 = %g\n",Q2_GeV2);
+    bbba05_(&Q2_GeV2, &GPE, &GPM, &GNE, &GNM);
+    // printf("qedifcrs.c GPE GPM GNE GNM = %g %g %g %g\n",GPE   , GPM  ,  GNE,
+    // GNM);
+    FGE = GPE - GNE;
+    FGM = GPM - GNM;
+  } else if ((nemdls_.mdlqe % 10) == 3) {
+    // printf("qedifcrs.c Q2_GeV2 = %g\n",Q2_GeV2);
+    bbba07_(&Q2_GeV2, &GPE, &GPM, &GNE, &GNM);
+    // printf("qedifcrs.c GPE GPM GNE GNM = %g %g %g %g\n",GPE   , GPM  ,  GNE,
+    // GNM);
+    FGE = GPE - GNE;
+    FGM = GPM - GNM;
+  } else if ((nemdls_.mdlqe % 10) == 1) {
+    FGE = pow(1. + Q2_GeV2 / ((Mvec) * (Mvec)), -2.);
+    FGM = (1. + 3.71) * FGE;
   } else {
-    printf("qedifcrs: Unknown MDLQE = %d\n",nemdls_.mdlqe);
+    printf("qedifcrs: Unknown MDLQE = %d\n", nemdls_.mdlqe);
   }
 
-  //Meson Exchange Current, also in dnelsq2.F
-  mec = ((nemdls_.mdlqe)/100);
+  // Meson Exchange Current, also in dnelsq2.F
+  mec = ((nemdls_.mdlqe) / 100);
   // printf("mec = %d\n",mec);
-  if (mec==1) FGM = FGM * sqrt(1+TRCORA * Q2 * exp( -1*Q2 / TRCORB ));
+  if (mec == 1)
+    FGM = FGM * sqrt(1 + TRCORA * Q2_GeV2 * exp(-1 * Q2_GeV2 / TRCORB));
 
-  F1 = (FGE + Q2*FGM/(XMNc*XMNc)/4.) / (1. + Q2/(XMNc*XMNc)/4.);
-  F2 = 0.5*(FGE-FGM)/XMNc/(1.+Q2/(XMNc*XMNc)/4.);
+  F1 = (FGE + Q2_GeV2 * FGM / (XMNc * XMNc) / 4.) /
+       (1. + Q2_GeV2 / (XMNc * XMNc) / 4.);
+  F2 = 0.5 * (FGE - FGM) / XMNc / (1. + Q2_GeV2 / (XMNc * XMNc) / 4.);
 
-  if (FF_model == 1 || FF_model == 0) {
-    //    printf("qedifcrs: axial dipole FF_model = %d\n",FF_model);
-    // FA = -1.232*pow(1.+Q2/(Maxial*Maxial), -2.);
-    FA = -1.267*pow(1.+Q2/((Maxial)*(Maxial)), -2.);
-  } else if (FF_model == 2) {
-    //    printf("qedifcrs: bbba07 BBBA07 FF_model = %d\n",FF_model);
-    // FA = faBBBA07(Q2/1.e+6);
-    FA = faBBBA07(Q2);
-  } else if (FF_model == 3){
-    //    printf("qedifcrs: 2 component FF_model = %d\n", FF_model);
-    FA = fa2COMP(Q2);
-  } else if (FF_model == 4){
-    //    printf("qedifcrs: 3 component FF_model = %d\n", FF_model);
-    FA = fa3COMP(Q2);
-  } else if (FF_model == 5){
-    //printf("qedifcrs: Z Expansation FF_model = %d\n", FF_model);
-    FA = faZEXP(Q2);
-  } else {
-    printf("qedifcrs: Unknown FF_model = %d\n",FF_model);
-    exit(1);
-  }
+  FA = elaxff_(&Q2_GeV2, &isnc, &nupdgsign);
 
-  //printf("qedifcrs: FA = %f\n",FA);
-  
-  //FP = 2.*XMNc*FA/(Q2 + 139.57*139.57);
-  FP = fperr * 2.*XMNc*FA/(Q2 + 0.13957*0.13957); 
+  // printf("qedifcrs: FA = %f\n",FA);
 
-  //Second-class currents
-  FV3 = sccv*(-1.)*pow(1. + Q2/((Mvec)*(Mvec)), -2.);
-  FA3 = scca * FA ;
+  // FP = 2.*XMNc*FA/(Q2_GeV2 + 139.57*139.57);
+  FP = fperr * 2. * XMNc * FA / (Q2_GeV2 + 0.13957 * 0.13957);
 
+  // Second-class currents
+  FV3 = sccv * (-1.) * pow(1. + Q2_GeV2 / ((Mvec) * (Mvec)), -2.);
+  FA3 = scca * FA;
 
   //--- T parameters,still need to add SCC here
-  T1 = 0.5*Q2*pow(F1- 2.*XMNc*F2, 2.) + (2.*XMNc*XMNc + .5*Q2)*FA*FA;
-  T2 = 2.*XMNc*XMNc*(F1*F1 + Q2*(F2*F2 + FA3*FA3) + FA*FA);
-//  TA = XMNc*XMNc*(2.*XMNc*F1*F2 + (.5*Q2 - 2.*XMNc*XMNc)*F2*F2 
-//				-2.*XMNc*FA*FP + .5*Q2*FP*FP);
-// add second class currents
-  //TA = XMNc*XMNc*(2.*XMNc*F1*F2 + (.5*Q2 - 2.*XMNc*XMNc)*(F2*F2 - FV3*FV3)
-  //				-2.*XMNc*FA*FP + .5*Q2*FP*FP
-  //              + FA3*(2*XMNc*FA - Q2*FP) + FV3*(2*XMNc*F1 + Q2*F2));
-  TA = XMNc*XMNc*(2*XMNc*F1*F2 + (.5*Q2-2*XMNc*XMNc)*F2*F2 
-		  + FV3*FV3*(0.5*Q2+2*XMNc*XMNc) -2.*XMNc*FA*FP + 0.5*Q2*FP*FP
-              + FA3*(2*XMNc*FA - Q2*FP) + FV3*(2*XMNc*F1 + Q2*F2)
-		  + 0.5*Q2*FA3*FA3);
-  TB = -.5*T2 - XMNc*XMNc*FV3*(2*XMNc*F1 + F2*Q2) - XMNc*XMNc*FA3*(2*XMNc*FA - Q2*FP);
-  T8 = 2.*XMNc*XMNc*FA*(F1- 2.*XMNc*F2);
+  T1 = 0.5 * Q2_GeV2 * pow(F1 - 2. * XMNc * F2, 2.) +
+       (2. * XMNc * XMNc + .5 * Q2_GeV2) * FA * FA;
+  T2 = 2. * XMNc * XMNc * (F1 * F1 + Q2_GeV2 * (F2 * F2 + FA3 * FA3) + FA * FA);
+  //  TA = XMNc*XMNc*(2.*XMNc*F1*F2 + (.5*Q2_GeV2 - 2.*XMNc*XMNc)*F2*F2
+  //				-2.*XMNc*FA*FP + .5*Q2_GeV2*FP*FP);
+  // add second class currents
+  // TA = XMNc*XMNc*(2.*XMNc*F1*F2 + (.5*Q2_GeV2 - 2.*XMNc*XMNc)*(F2*F2 -
+  // FV3*FV3) 				-2.*XMNc*FA*FP + .5*Q2_GeV2*FP*FP
+  //              + FA3*(2*XMNc*FA - Q2_GeV2*FP) + FV3*(2*XMNc*F1 +
+  //              Q2_GeV2*F2));
+  TA = XMNc * XMNc *
+       (2 * XMNc * F1 * F2 + (.5 * Q2_GeV2 - 2 * XMNc * XMNc) * F2 * F2 +
+        FV3 * FV3 * (0.5 * Q2_GeV2 + 2 * XMNc * XMNc) - 2. * XMNc * FA * FP +
+        0.5 * Q2_GeV2 * FP * FP + FA3 * (2 * XMNc * FA - Q2_GeV2 * FP) +
+        FV3 * (2 * XMNc * F1 + Q2_GeV2 * F2) + 0.5 * Q2_GeV2 * FA3 * FA3);
+  TB = -.5 * T2 - XMNc * XMNc * FV3 * (2 * XMNc * F1 + F2 * Q2_GeV2) -
+       XMNc * XMNc * FA3 * (2 * XMNc * FA - Q2_GeV2 * FP);
+  T8 = 2. * XMNc * XMNc * FA * (F1 - 2. * XMNc * F2);
 
   //--- B parameters
-  //  if (Q == 0.){
-  //    printf("returning 0 because Q == 0.");
+  //  if (Q_GeV == 0.){
+  //    printf("returning 0 because Q_GeV == 0.");
   //    return 0.;
   //  }
 
-  C = -WEF/Q;
-  D = Q2EF/(2.*Q*XMNc);
-  PFn = pow(2.*(Nnum)/((Znum)+(Nnum)), 1./3.)*(PFermi);
-  X0 = 3.*(Nnum)/(4.*Q*pow(PFn,3.));
+  C = -WEF / Q_GeV;
+  D = Q2EF / (2. * Q_GeV * XMNc);
+  PFn = pow(2. * (Nnum) / ((Znum) + (Nnum)), 1. / 3.) * (PFermi);
+  X0 = 3. * (Nnum) / (4. * Q_GeV * pow(PFn, 3.));
   if (WEF > 0.) {
-	DBLS = 1.-C*C+D*D;
-	if (DBLS <= 0.) DBLS = 0.;
-	S1 = XMNc*(C*D+sqrt(DBLS))/(1.-C*C);
-	S2 = kappa*(EU - WEF);
-	EL = fmax(S1, S2);
+    DBLS = 1. - C * C + D * D;
+    if (DBLS <= 0.)
+      DBLS = 0.;
+    S1 = XMNc * (C * D + sqrt(DBLS)) / (1. - C * C);
+    S2 = kappa * (EU - WEF);
+    EL = fmax(S1, S2);
   }
 
   //  if (WEF < 0. || EU < EL){
@@ -265,50 +206,52 @@ qedifcrs_(int *ip, int *ccnc, float *energy, float *elep, float *coslep)
 
   //    if ( EU < EL)
   //      printf("return 0 because EU < EL\n");
-  //    
+  //
   //    return 0.;
 
   //  }
 
-  B0 = X0*(EU - EL+AP*log((EU-(Ebind))/(EL-(Ebind)))
-		   +BP*log((EU-(Ebind)+W)/(EL-(Ebind)+W)));
-  B1 = X0/XMNc*(.5*(EU*EU-EL*EL)+
-			   AP*(EU-EL+(Ebind)*log((EU- (Ebind))/(EL-(Ebind))))
-			   +BP*((EU-EL)
-					+((Ebind)-W)*log((EU-(Ebind)+W)/(EL-(Ebind)+W))));
-  B2 = X0/(XMNc*XMNc)*(1./3.*(pow(EU,3.)-pow(EL,3.))
-					 +AP*(.5*(EU*EU-EL*EL)+(Ebind)*(EU-EL)
-						  +(Ebind)*(Ebind)*log((EU-(Ebind))/(EL-(Ebind))))
-					 +BP*(.5*(EU*EU-EL*EL)+((Ebind)-W)*
-						  (EU-EL)+pow((Ebind)-W,2.)
-						  *log((EU-(Ebind)+W)/(EL-(Ebind)+W))));
-  
+  B0 = X0 * (EU - EL + AP * log((EU - (Ebind)) / (EL - (Ebind))) +
+             BP * log((EU - (Ebind) + W_GeV) / (EL - (Ebind) + W_GeV)));
+  B1 = X0 / XMNc *
+       (.5 * (EU * EU - EL * EL) +
+        AP * (EU - EL + (Ebind)*log((EU - (Ebind)) / (EL - (Ebind)))) +
+        BP * ((EU - EL) + ((Ebind)-W_GeV) * log((EU - (Ebind) + W_GeV) /
+                                                (EL - (Ebind) + W_GeV))));
+  B2 = X0 / (XMNc * XMNc) *
+       (1. / 3. * (pow(EU, 3.) - pow(EL, 3.)) +
+        AP * (.5 * (EU * EU - EL * EL) + (Ebind) * (EU - EL) +
+              (Ebind) * (Ebind)*log((EU - (Ebind)) / (EL - (Ebind)))) +
+        BP * (.5 * (EU * EU - EL * EL) + ((Ebind)-W_GeV) * (EU - EL) +
+              pow((Ebind)-W_GeV, 2.) *
+                  log((EU - (Ebind) + W_GeV) / (EL - (Ebind) + W_GeV))));
+
   //--- A parameters
   A1 = B0;
   A2 = B2 - B0;
-  A3 = C*C*B2 + 2.*C*D*B1+D*D*B0;
-  A4 = B2 - 2.*(Ebind)/XMNc*B1+(Ebind)*(Ebind)/(XMNc*XMNc)*B0;
-  A5 = C*B2+(D-(Ebind)*C/XMNc)*B1-(Ebind)*D/XMNc*B0;
-  A6 = C*B1+D*B0;
-  A7 = B1-(Ebind)/XMNc*B0;
+  A3 = C * C * B2 + 2. * C * D * B1 + D * D * B0;
+  A4 = B2 - 2. * (Ebind) / XMNc * B1 + (Ebind) * (Ebind) / (XMNc * XMNc) * B0;
+  A5 = C * B2 + (D - (Ebind)*C / XMNc) * B1 - (Ebind)*D / XMNc * B0;
+  A6 = C * B1 + D * B0;
+  A7 = B1 - (Ebind) / XMNc * B0;
 
-  //--- W parameters
-  W1 = A1*T1+.5*(A2-A3)*T2;
-  W2 = (A4+2*W/Q*A5+W*W/(Q*Q)*A3+.5*Q2/(Q*Q)
-		*(A2-A3))*T2;
-  WA = 1./(Q*Q)*(1.5*A3-.5*A2)*T2
-	+1./(XMNc*XMNc)*A1*TA + 2./(XMNc*Q)*A6*TB;
-  WB = 1./XMNc * (A7+W/Q*A6)*TB
-	+ W/(Q*Q)*(1.5*A3-.5*A2+Q/W*A5)*T2;
-  W8 = NUSIG/XMNc*(A7+W/Q*A6)*T8;
+  //--- W_GeV parameters
+  W1 = A1 * T1 + .5 * (A2 - A3) * T2;
+  W2 = (A4 + 2 * W_GeV / Q_GeV * A5 + W_GeV * W_GeV / (Q_GeV * Q_GeV) * A3 +
+        .5 * Q2_GeV2 / (Q_GeV * Q_GeV) * (A2 - A3)) *
+       T2;
+  WA = 1. / (Q_GeV * Q_GeV) * (1.5 * A3 - .5 * A2) * T2 +
+       1. / (XMNc * XMNc) * A1 * TA + 2. / (XMNc * Q_GeV) * A6 * TB;
+  WB = 1. / XMNc * (A7 + W_GeV / Q_GeV * A6) * TB +
+       W_GeV / (Q_GeV * Q_GeV) * (1.5 * A3 - .5 * A2 + Q_GeV / W_GeV * A5) * T2;
+  W8 = nupdgsign / XMNc * (A7 + W_GeV / Q_GeV * A6) * T8;
 
   //--- Differential cross section
-  COX = PLEP/(*elep)*(*coslep);
-  DSIG = COSB2*pow(GF*HC,2.)*(*elep)*PLEP/PI *.5 *
-	(W2*(1.+COX)+(2.*W1+XM*XM*WA)*(1.-COX)
-	 -2.*W8*((*energy)+(*elep))*(1.-COX)
-	 +(WB+W8)*2.*XM*XM/(*elep));
+  COX = PLEP / (*elep_GeV) * (*coslep);
+  DSIG = COSB2 * pow(GF * HC, 2.) * (*elep_GeV) * PLEP / PI * .5 *
+         (W2 * (1. + COX) + (2. * W1 + Mlep_GeV * Mlep_GeV * WA) * (1. - COX) -
+          2. * W8 * ((*energy_GeV) + (*elep_GeV)) * (1. - COX) +
+          (WB + W8) * 2. * Mlep_GeV * Mlep_GeV / (*elep_GeV));
 
   return DSIG;
-
 }
--- src/neututils/calc_ccqe_crs/CrossSection.cc
+++ src/neututils/calc_ccqe_crs/CrossSection.cc
@@ -9,16 +9,14 @@ using namespace std;
 
 # include "CrossSection.hh"
 # include "clenshaw_curtis.hh"
-//# include "bbba07.cc"
 
 //#define DEBUG
 
 extern "C"{
   double bbba05_(double *,double *,double *,double *,double *);
-  int bbba07_(double *x, 
+  int bbba07_(double *x,
 			  double *gep, double *gmp, double *gen, double *gmn);
-  //  double faBBBA07(double);
-  double faBBBA07(double, double);
+  double elaxff_(double *Q2_GeV, bool *nc, int *sgn);
 }
 
 
@@ -26,7 +24,7 @@ CrossSection::CrossSection(double Pfermi, int nu_type, int nu_sign, double XAmas
   : PFermi(Pfermi), ITYPE(nu_type), NUSIG(nu_sign)
 {
 #ifdef DEBUG
-  cout << "Neutrino type= " << ITYPE << " : " << NUSIG 
+  cout << "Neutrino type= " << ITYPE << " : " << NUSIG
        << ", Fermi momentum= " << PFermi << endl;
 #endif
   Initialize(XAmass);
@@ -43,7 +41,7 @@ double CrossSection::DifferentialCrossSection(const double energy) {
   double const XMN = 939.6;
   double const DMN = 0.;
   double const EBF = 0.;
-  
+
   //--- Local variables
   double W,Q,Q2,PLEP,WEF,QVEC2,AP,BP,Q2EF;
   double FGE,FGM,F1,F2,FA,FP;
@@ -106,17 +104,13 @@ double CrossSection::DifferentialCrossSection(const double energy) {
   F1 = (FGE + Q2*FGM/(XMN*XMN)/4.) / (1. + Q2/(XMN*XMN)/4.);
   F2 = 0.5*(FGE-FGM)/XMN/(1.+Q2/(XMN*XMN)/4.);
 
-  if (A_FF_model == 0) {
-	//    FA = -1.232*pow(1.+Q2/(Maxial*Maxial), -2.);
-	FA = -1.267*pow(1.+Q2/(XMaxial*XMaxial), -2.);
-  } else if (A_FF_model == 1) {
-    FA = faBBBA07(Q2/1.e+6,XMaxial/1.e3);
-  }else{
-	cout << "Unknown A_FF_model parameter " << A_FF_model << endl;
-	exit(1);
-  }
+  bool isnc = false;
+  //Only matters for isnc == true
+  int nupdg_dummy = 0;
+  FA = elaxff_(&Q2,&isnc,&nupdg_dummy);
+
   FP = 2.*XMN*FA/(Q2 + 139.57*139.57);
-      
+
   //--- T parameters
   T1 = 0.5*Q2*pow(F1- 2.*XMN*F2, 2.) + (2.*XMN*XMN + .5*Q2)*FA*FA;
   T2 = 2.*XMN*XMN*(F1*F1 + Q2*F2*F2 + FA*FA);
@@ -124,7 +118,7 @@ double CrossSection::DifferentialCrossSection(const double energy) {
                 -2.*XMN*FA*FP + .5*Q2*FP*FP);
   TB = -.5*T2;
   T8 = 2.*XMN*XMN*FA*(F1- 2.*XMN*F2);
-    
+
   //--- B parameters
   if (Q == 0.) return 0.;
 
@@ -177,7 +171,7 @@ double CrossSection::DifferentialCrossSection(const double energy) {
   COX = PLEP/elep*coslep;
   DSIG = COSB2*pow(GF*HC,2.)*elep*PLEP/PI *.5 *
     (W2*(1.+COX)+(2.*W1+XM*XM*WA)*(1.-COX)
-     -2.*W8*(energy+elep)*(1.-COX) 
+     -2.*W8*(energy+elep)*(1.-COX)
      +(WB+W8)*2.*XM*XM/elep);
 
   return DSIG;
@@ -203,12 +197,12 @@ double CrossSection::TotalCrossSection(const double energy, const int nloop) {
   for ( dim = 0; dim < dim_num; dim++ ) {
     order_1d[dim] = nloop;
   }
-  
+
   order_nd = i4vec_product ( dim_num, order_1d ); // order_1d[0]*order_1d[1]
-  
+
   point  = new double[dim_num*order_nd];
   weight = new double[order_nd];
-  
+
   clenshaw_curtis_compute_nd ( dim_num, order_1d, point, weight );
 
   for ( order = 0; order < order_nd; order++ ) {
@@ -227,7 +221,7 @@ double CrossSection::TotalCrossSection(const double energy, const int nloop) {
   delete [] order_1d;
   delete [] point;
   delete [] weight;
-  
+
   return weight_sum*1.e+12/8./4.; // /#of nucleon = 8/[-1,1]^2 = 4
 
 }
