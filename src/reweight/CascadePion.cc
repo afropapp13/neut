4e2f4b3d9532cee5da253b702d540ff2cdd96c33
Luke Pickering <picker24@msu.edu>
2021-05-04 02:53:55 +0000

    Pion FSI Reweight now checks if dial values are sensible during reconfigure and crashes hard if they aren't. You've been warned.

--- src/reweight/CascadePion.cc
+++ src/reweight/CascadePion.cc
@@ -15,15 +15,33 @@ namespace neut {
 namespace rew {
 
 CascadePionEngine::CascadePionEngine() {
-  PionFSI_AbsProb = RegisterDial("PionFSI_AbsProb", neffpr_.fefabs, 0.432, 0.432);
-  PionFSI_QELowMomProb = RegisterDial("PionFSI_QELowMomProb", neffpr_.fefqe, 0.313, 0.313);
-  PionFSI_CExLowMomProb = RegisterDial("PionFSI_CExLowMomProb", neffpr_.fefcx, 0.305, 0.305);
-  PionFSI_QEHighMomProb = RegisterDial("PionFSI_QEHighMomProb", neffpr_.fefqeh, 0.859, 0.859);
-  PionFSI_CExHighMomProb = RegisterDial("PionFSI_CExHighMomProb", neffpr_.fefcxh);
-  PionFSI_InelProb = RegisterDial("PionFSI_InelProb", neffpr_.fefinel, 1.101, 1.101);
+  PionFSI_AbsProb =
+      RegisterDial("PionFSI_AbsProb", neffpr_.fefabs, 0.432, 0.432);
+  PionFSI_QELowMomProb =
+      RegisterDial("PionFSI_QELowMomProb", neffpr_.fefqe, 0.313, 0.313);
+  PionFSI_CExLowMomProb =
+      RegisterDial("PionFSI_CExLowMomProb", neffpr_.fefcx, 0.305, 0.305);
+  PionFSI_QEHighMomProb =
+      RegisterDial("PionFSI_QEHighMomProb", neffpr_.fefqeh, 0.859, 0.859);
+  PionFSI_CExHighMomProb =
+      RegisterDial("PionFSI_CExHighMomProb", neffpr_.fefcxh);
+  PionFSI_InelProb =
+      RegisterDial("PionFSI_InelProb", neffpr_.fefinel, 1.101, 1.101);
 }
 
-void CascadePionEngine::Reconfigure(void) { ApplyTweaks(); }
+void CascadePionEngine::Reconfigure(void) {
+  ApplyTweaks();
+  for (auto &dial : fDials) {
+    if (dial.second.ToValue < 0) {
+      std::cout << "[ERROR]: Dial: " << NSyst::DialAsString(dial.first)
+                << " set unphysical value: " << dial.second.ToValue
+                << " (CV: " << dial.second.FromValue
+                << ", Uncert: " << dial.second.GetOneSigma(dial.second.Tweak)
+                << ", Tweak: " << dial.second.Tweak << ")" << std::endl;
+      abort();
+    }
+  }
+}
 
 double CascadePionEngine::CalcWeight() {
 
