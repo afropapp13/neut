258f21b23373aae515c9edad7e2d8d3855c40467
cwret <c.wret@rochester.edu>
2022-01-11 16:43:01 +0000

    Commit std:: qualifier for isnan in all instance of the call

--- src/n1p1h/src/N1p1h2d.C
+++ src/n1p1h/src/N1p1h2d.C
@@ -396,8 +396,7 @@ void N1p1h2d::ComputeIntegrals(int nuclei){
 
 	  double xsl = DoubleDifferential(id,nuclei,Enu,tl,xcos);
 
-	  //	  if( xsl < 0. || std::isnan(xsl)  ) xsl=0.;
-	  if( xsl < 0. || isnan(xsl)  ) xsl=0.;
+	  if( xsl < 0. || std::isnan(xsl)  ) xsl=0.;
 
 	  xs =  xsl*intfactor4;
 	  xs2 =  xsl*xsl*intfactor4;
