61f2f60e9d57818a9a42902f8e61bcd504c07c7c
Yoshinari Hayato <hayato@suketto.icrr.u-tokyo.ac.jp>
2022-02-13 09:54:42 +0900

    Add cards which is supported in the Local Fermigas model to generate cross-section table easily.

---
+++ src/n1p1h/Cards/xsec_gen.sh
@@ -0,0 +1,31 @@
+./neutroot neut_5.5.0.CCQE_xsec_10B.card    neut_5.5.0.CCQE_xsec_10B.root > neut_5.5.0.CCQE_xsec_10B.log &
+./neutroot neut_5.5.0.CCQE_xsec_11B.card    neut_5.5.0.CCQE_xsec_11B.root > neut_5.5.0.CCQE_xsec_11B.log &
+./neutroot neut_5.5.0.CCQE_xsec_12C.card	neut_5.5.0.CCQE_xsec_12C.root > neut_5.5.0.CCQE_xsec_12C.log &
+./neutroot neut_5.5.0.CCQE_xsec_13C.card	neut_5.5.0.CCQE_xsec_13C.root > neut_5.5.0.CCQE_xsec_13C.log &
+./neutroot neut_5.5.0.CCQE_xsec_14N.card	neut_5.5.0.CCQE_xsec_14N.root > neut_5.5.0.CCQE_xsec_14N.log &
+./neutroot neut_5.5.0.CCQE_xsec_15N.card	neut_5.5.0.CCQE_xsec_15N.root > neut_5.5.0.CCQE_xsec_15N.log &
+./neutroot neut_5.5.0.CCQE_xsec_16O.card	neut_5.5.0.CCQE_xsec_16O.root > neut_5.5.0.CCQE_xsec_16O.log &
+./neutroot neut_5.5.0.CCQE_xsec_17O.card	neut_5.5.0.CCQE_xsec_17O.root > neut_5.5.0.CCQE_xsec_17O.log &
+./neutroot neut_5.5.0.CCQE_xsec_18O.card	neut_5.5.0.CCQE_xsec_18O.root > neut_5.5.0.CCQE_xsec_18O.log &
+./neutroot neut_5.5.0.CCQE_xsec_19F.card	neut_5.5.0.CCQE_xsec_19F.root > neut_5.5.0.CCQE_xsec_19F.log &
+./neutroot neut_5.5.0.CCQE_xsec_20Ne.card	neut_5.5.0.CCQE_xsec_20Ne.root >neut_5.5.0.CCQE_xsec_20Ne.log &
+./neutroot neut_5.5.0.CCQE_xsec_23Na.card	neut_5.5.0.CCQE_xsec_23Na.root >neut_5.5.0.CCQE_xsec_23Na.log &
+./neutroot neut_5.5.0.CCQE_xsec_27Al.card	neut_5.5.0.CCQE_xsec_27Al.root >neut_5.5.0.CCQE_xsec_27Al.log &
+./neutroot neut_5.5.0.CCQE_xsec_28Si.card	neut_5.5.0.CCQE_xsec_28Si.root >neut_5.5.0.CCQE_xsec_28Si.log &
+./neutroot neut_5.5.0.CCQE_xsec_29Si.card	neut_5.5.0.CCQE_xsec_29Si.root >neut_5.5.0.CCQE_xsec_29Si.log &
+./neutroot neut_5.5.0.CCQE_xsec_30Si.card	neut_5.5.0.CCQE_xsec_30Si.root >neut_5.5.0.CCQE_xsec_30Si.log &
+./neutroot neut_5.5.0.CCQE_xsec_35Cl.card	neut_5.5.0.CCQE_xsec_35Cl.root >neut_5.5.0.CCQE_xsec_35Cl.log &
+./neutroot neut_5.5.0.CCQE_xsec_40Ar.card	neut_5.5.0.CCQE_xsec_40Ar.root >neut_5.5.0.CCQE_xsec_40Ar.log &
+./neutroot neut_5.5.0.CCQE_xsec_46Ti.card	neut_5.5.0.CCQE_xsec_46Ti.root >neut_5.5.0.CCQE_xsec_46Ti.log &
+./neutroot neut_5.5.0.CCQE_xsec_47Ti.card	neut_5.5.0.CCQE_xsec_47Ti.root >neut_5.5.0.CCQE_xsec_47Ti.log &
+./neutroot neut_5.5.0.CCQE_xsec_48Ti.card	neut_5.5.0.CCQE_xsec_48Ti.root >neut_5.5.0.CCQE_xsec_48Ti.log &
+./neutroot neut_5.5.0.CCQE_xsec_49Ti.card	neut_5.5.0.CCQE_xsec_49Ti.root >neut_5.5.0.CCQE_xsec_49Ti.log &
+./neutroot neut_5.5.0.CCQE_xsec_50Ti.card	neut_5.5.0.CCQE_xsec_50Ti.root >neut_5.5.0.CCQE_xsec_50Ti.log &
+./neutroot neut_5.5.0.CCQE_xsec_56Fe.card	neut_5.5.0.CCQE_xsec_56Fe.root >neut_5.5.0.CCQE_xsec_56Fe.log &
+./neutroot neut_5.5.0.CCQE_xsec_59Co.card	neut_5.5.0.CCQE_xsec_59Co.root >neut_5.5.0.CCQE_xsec_59Co.log &
+./neutroot neut_5.5.0.CCQE_xsec_63Cu.card	neut_5.5.0.CCQE_xsec_63Cu.root >neut_5.5.0.CCQE_xsec_63Cu.log &
+./neutroot neut_5.5.0.CCQE_xsec_64Zn.card	neut_5.5.0.CCQE_xsec_64Zn.root >neut_5.5.0.CCQE_xsec_64Zn.log &
+./neutroot neut_5.5.0.CCQE_xsec_65Cu.card	neut_5.5.0.CCQE_xsec_65Cu.root >neut_5.5.0.CCQE_xsec_65Cu.log &
+./neutroot neut_5.5.0.CCQE_xsec_207Pb.card  neut_5.5.0.CCQE_xsec_207Pb.root >neut_5.5.0.CCQE_xsec_207Pb.log &
+./neutroot neut_5.5.0.CCQE_xsec_208Pb.card  neut_5.5.0.CCQE_xsec_208Pb.root >neut_5.5.0.CCQE_xsec_208Pb.log &
+./neutroot neut_5.5.0.CCQE_xsec_209Bi.card  neut_5.5.0.CCQE_xsec_209Bi.root >neut_5.5.0.CCQE_xsec_209Bi.log &
